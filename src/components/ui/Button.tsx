6fb8fb675377e50a782006742e82ed08460ad5ad
PhantomHerald <ankomahgerald@gmail.com>
2025-11-11 10:23:13 +0000

    feat: add global styles and layout for the application
    - Introduced global CSS styles with Tailwind CSS integration.
    - Set up dark mode support for background and foreground colors.
    - Created a RootLayout component to wrap the application with metadata and styles.
    
    feat: implement Home page structure
    
    - Developed the Home page with a centered layout and introductory content.
    - Added links to templates and documentation for user guidance.
    - Included Next.js and Vercel logos with appropriate styling.
    
    feat: create reusable Button component with GSAP animations
    
    - Implemented a Button component that supports hover animations using GSAP.
    - Added props for customization, including right icon and aria attributes.
    
    fix: update hero section to use client components
    
    - Modified the hero section to utilize client-side rendering with Next.js.
    
    feat: add utility function for class name management
    
    - Created a utility function `cn` to merge Tailwind CSS class names using clsx and tailwind-merge.

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,63 @@
+import { ButtonHTMLAttributes, ReactNode, forwardRef, useRef } from "react";;
+import {cn} from "@/src/utils/tailwind";
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  ariaId?: string;
+  animateOnHover?: boolean;
+  rightIcon?: ReactNode;
+}
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+    (
+        {
+            children,
+            rightIcon,
+            animateOnHover = true,
+            className = "",
+            ariaId,
+            ...rest
+        },
+        ref,
+    ) => {
+        const scope = useRef<HTMLDivElement>(null);
+        const duration = 0.3;
+
+        const { contextSafe } = useGSAP(() => {}, {scope});
+
+        const onHover = contextSafe(() => {
+            if (!animateOnHover) return;
+            gsap.fromTo(
+                "button",
+                {scale : 1},
+                {scale : 1.05, ease: "back.out(2.5)", duration}
+            );
+        });
+
+        const onleave = contextSafe(() => {
+            if (!animateOnHover) return;
+            gsap.to("button", {scale : 1,  duration});
+        });
+
+        return(
+            <div ref = {scope} > 
+            <button ref={ref}
+              {...(!rest["aria-label"] && !rest["aria-labelledby"]
+            ? { "aria-labelledby": ariaId }
+            : {})}
+          {...rest}
+          className={cn("py-4 px-8 font-semibold bg-primary text-white rounded-full flex items-center justify-center gap-2", className)}
+            onMouseEnter={onHover}
+            onMouseLeave={onleave}
+        >
+       <span id={ariaId} className="shrink-0"> {children} </span>
+        {rightIcon && <span>{rightIcon}</span>}
+                </button>
+            </div>
+        )
+    },
+);
+
+Button.displayName = "Button";
+export default Button;
---
+++ src/sections/hero/index.tsx
@@ -0,0 +1,2 @@
+"use client";
+import link from "next/link";
